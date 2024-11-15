1b5c39e21435c3b0c61fa72a27904823105ade90
Oana Mancu <oanamancu@gmail.com>
2024-11-16 01:14:33 +0200

    feat: server & client side

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,18 +2,56 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+const server = app.listen(port, () => console.log(`Server running in port ${port}`));
+
+//Routes
+
+function checkData(data) {
+    const keys = Object.keys(data);
+    const lookFor = ['temperature', 'date', 'userResponse'];
+    for(const key of lookFor) {
+        if (!keys.includes(key)) {
+            return false;
+        }
+    } 
+    return true;
+}
+
+app.get('/data', (req, res) => {
+    console.log('projectData: ' + Object.values(projectData));
+    res.send(projectData);
+});
+
+app.post('/data', (req, res) => { 
+    if(checkData(req.body)) {
+        projectData['temperature'] = req.body.temperature;
+        projectData['date'] = req.body.date;
+        projectData['userResponse'] = req.body.userResponse;
+        res.send(projectData);
+    }
+    else { 
+        throw new Error(`no ccorrect data sent! 
+            The format should be: {temperature: temperatureValue, date: dateValue, userResponse: userRespondeValue}`);
+    }
+});
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,73 @@
 /* Global Variables */
 
+const weatherBaseURL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
+const serverBaseURL = 'http://localhost:3000';
+
+//https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Bucharest?unitGroup=metric&include=current&key=xyz&contentType=json
+const apiKey = "key=MYAT2B7YH4SZ45VKR3Z9YS7D4"; 
+/*window.addEventListener('load', async () => {
+    apiKey = `key=${(await (async () => await fetch(`../.env`).then(response => response.json()))()).API_KEY}`;
+})*/
+
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+function getDate() {
+    let d = new Date();
+    let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+    return newDate;
+}
+
+async function fetchWeather(weatherBaseURL, apiKey, zip) {
+    const urlRetrieveOptins = 'unitGroup=metric&include=current';
+    const urlContentType = 'contentType=json';
+    const url = `${weatherBaseURL}${zip}?${urlRetrieveOptins}&${apiKey}&${urlContentType}`;
+    const response = await fetch(url);
+    try {
+        return (await response.json()).currentConditions.temp;
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+async function postEntry(path, data) {
+    const response = await fetch(path, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    try {
+        return await response.json();
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+async function getEntry(path) {
+    const response = await fetch(path);
+    try {
+        return await response.json();
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+
+async function generateWeatherEntry() {
+    return fetchWeather(weatherBaseURL, apiKey, document.querySelector('#zip').value)
+    .then(temperature => postEntry(`${serverBaseURL}/data`,{temperature: temperature, date: getDate(), userResponse: document.querySelector('#feelings').value}))
+    .then(res => getEntry(`${serverBaseURL}/data`))
+    .then(data => {
+        document.querySelector('#content').innerText = data.userResponse;
+        document.querySelector('#temp').innerText = data.temperature;
+        document.querySelector('#date').innerText = data.date;
+    })
+    .catch(err => console.log(err));
+}
+
+function createNewEntry() {
+    generateWeatherEntry();
+}
+
+document.querySelector('#generate').addEventListener('click', createNewEntry);
